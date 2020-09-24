78882b587f5260913670f30f128d80810baf3660
Tibs088 <tibs088@gmail.com>
2020-09-24 17:25:25 +0100

    arf arf done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # arf_arf_bot
+
+Arf Arf is a bot for random barks to be used
--- index.js
+++ index.js
@@ -4,3 +4,19 @@ const config = require("./config.json");
 const client = new Discord.Client();
 
 client.login(config.BOT_TOKEN);
+
+const prefix = '€'
+
+client.on('message', (message) => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(' ');
+  const command = args.shift().toLowerCase();
+
+  if (command === 'arf') {
+    const timeTaken = Date.now() - message.createdTimestamp;
+    message.reply(`Arf Arf!     Time Taken:${timeTaken}ms`)
+  }
+})
