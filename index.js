45388f95b34951dabc881cf1b4b4cb7ae24db32c
Tibs088 <tibs088@gmail.com>
2020-09-24 18:31:31 +0100

    omg done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const config = require("./config.json");
+const ytdl = require('ytdl-core')
 
 const client = new Discord.Client();
 
@@ -7,16 +8,61 @@ client.login(config.BOT_TOKEN);
 
 const prefix = '€'
 
+client.once('ready', () => {
+  console.log('Ready!');
+});
+client.once('reconnecting', () => {
+  console.log('Reconnecting!');
+});
+client.once('disconnect', () => {
+  console.log('Disconnect!');
+});
+
+function arfArf(guild) {
+
+}
+
 client.on('message', (message) => {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
 
+  let isReady = true;
   const commandBody = message.content.slice(prefix.length);
   const args = commandBody.split(' ');
   const command = args.shift().toLowerCase();
 
-  if (command === 'arf') {
+  if (command === 'arf' && isReady) {
+    !isReady;
     const timeTaken = Date.now() - message.createdTimestamp;
-    message.reply(`Arf Arf!     Time Taken:${timeTaken}ms`)
+    message.reply(`Arf Arf!     Time Taken:${timeTaken}ms`);
+    let voiceChannel = message.member.voice.channel;
+    voiceChannel.join().then(connection => {
+      const dispatcher = connection.play(ytdl('https://www.youtube.com/watch?v=DTMvh4hFeA4'));
+      dispatcher.on('end', end => {
+        voiceChannel.leave();
+      })
+    }).catch((err) => {
+      console.log(err);
+    })
+
+    //arfArf();
+    isReady;
+  }
+  if (command === 'omg' && isReady) {
+    !isReady;
+    const timeTaken = Date.now() - message.createdTimestamp;
+    message.reply(`Oh my god, who the hell cares     Time Taken:${timeTaken}ms`);
+    let voiceChannel = message.member.voice.channel;
+    voiceChannel.join().then(connection => {
+      const dispatcher = connection.play(ytdl('https://www.youtube.com/watch?v=RAA1xgTTw9w'));
+      dispatcher.on('end', end => {
+        voiceChannel.leave();
+      })
+    }).catch((err) => {
+      console.log(err);
+    })
+
+    //arfArf();
+    isReady;
   }
 })
